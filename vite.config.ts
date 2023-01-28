1dfeb9935fc68b654ff05f53a157665d8fd9a6e6
Shinichi Okada <okada.shin@gmail.com>
2023-01-28 09:45:10 +0900

    fix: base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/svelte-starter/",
+  base: "/svelte-ts-starter/",
   plugins: [svelte()],
 })
